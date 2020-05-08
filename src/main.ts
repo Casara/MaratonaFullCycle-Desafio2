1af9be4b63f6f1e186791a07f6d72a36df0468d2
Rodrigo Casara <rodrigocasara@gmail.com>
2020-05-08 17:05:49 -0300

    :ok_hand: remove comments

--- src/main.ts
+++ src/main.ts
@@ -36,28 +36,3 @@ async function bootstrap() {
   await app.listen(port, '0.0.0.0');
 }
 bootstrap();
-
-/*
-
-https://www.npmjs.com/package/object-mapper
-https://github.com/typescript-eslint/typescript-eslint/tree/master/packages/eslint-plugin/docs/rules
-
-https://www.npmjs.com/~nestjscore
-https://github.com/nestjs/typescript-starter/blob/master/package.json
-https://github.com/nestjs/nest/tree/master/sample/10-fastify
-
-https://morioh.com/p/9dd19ecd36d4
-https://dev.to/vinodsr/nestjs-a-backend-nodejs-framework-for-the-enterprise-40m6
-
-https://www.codemag.com/Article/1907081/Nest.js-Step-by-Step
-https://www.codemag.com/Article/1909081/Nest.js-Step-by-Step-Part-2
-https://codemag.com/Article/2001081/Nest.js-Step-by-Step-Part-3-Users-and-Authentication
-
-https://www.techiediaries.com/nestjs-tutorial-rest-api-crud/
-
-
-https://blog.logrocket.com/containerized-development-nestjs-docker/
-https://blog.logrocket.com/introduction-to-lando/
-https://blog.logrocket.com/moving-from-scss-to-styled-components-advantages-and-caveats/
-
- */
